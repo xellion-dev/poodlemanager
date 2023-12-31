013b0dc04c4235031969fdbff5c8cf25672e7fed
xellion-dev <thegamingpenguinstgp@gmail.com>
2023-12-31 01:39:16 -0600

    modified:   main.go

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var pkgname string
+
+	fmt.Print("PoodleManager Version 1.0.0")
+	fmt.Scan(&pkgname)
+	fmt.Println("Package is", pkgname)
 }
